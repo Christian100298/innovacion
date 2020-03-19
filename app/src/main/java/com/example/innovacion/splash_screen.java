9c0cae9d40152b8207fd4586749529720e5ee5b7
Aurora-123 <43626318+Aurora-123@users.noreply.github.com>
2020-03-18 23:27:13 -0600

    diseño de layouts

---
+++ app/src/main/java/com/example/innovacion/splash_screen.java
@@ -0,0 +1,33 @@
+package com.example.innovacion;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class splash_screen extends AppCompatActivity {
+
+    private final int duration_splah = 5000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intend = new Intent(splash_screen.this,login.class);
+                startActivity(intend);
+                finish();
+            };
+        }, duration_splah);
+    }
+}
+
